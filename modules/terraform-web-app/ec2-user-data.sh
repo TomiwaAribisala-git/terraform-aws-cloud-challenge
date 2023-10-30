ae2905975b9f4a790ff5b624583ea3ae90e99820
unknown <tomiwaaribisala@gmail.com>
2023-10-30 17:54:57 +0100

    terraform cloud challenge modules

--- modules/terraform-web-app/ec2-user-data.sh
+++
@@ -1,9 +0,0 @@
-yum update -y
-yum install docker -y
-systemctl enable docker
-systemctl start docker
-systemctl enable docker
-systemctl start docker
-usermod -aG docker ec2-user
-echo tomiwa97 | docker login -u dckr_pat_Oj0GFrJbkW9P672jNvOhIMasKOM --password-stdin
-docker run -d -p 3000:3000 tomiwa97/node-project:latest
